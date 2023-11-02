b86d87dc668f7ab98f48faa612f6903a50e4b29b
Julia Volheim <143303084+jcvol@users.noreply.github.com>
2023-11-02 00:10:32 +0000

    style login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -6,91 +6,83 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useNavigate } from 'react-router-dom';
 
-function Login()
-{
+function Login() {
   var bp = require('./Path.js');
 
   const loginName = useRef();
   const loginPassword = useRef();
 
-  const [message,setMessage] = useState('');
+  const [message, setMessage] = useState('');
   const navigate = useNavigate();
 
-  const doLogin = async event => 
-  {
-      event.preventDefault();
+  const doLogin = async event => {
+    event.preventDefault();
 
-      const obj = {
-        username: loginName.current?.value,
-        password: loginPassword.current?.value
-      };
-      const js = JSON.stringify(obj);
+    const obj = {
+      username: loginName.current?.value,
+      password: loginPassword.current?.value
+    };
+    const js = JSON.stringify(obj);
 
-      try
-      {    
-          const response = await fetch(bp.buildPath('api/login'),
-              {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+    try {
+      const response = await fetch(bp.buildPath('api/login'),
+        { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
 
-          var res = JSON.parse(await response.text());
-          var storage = require('../tokenStorage.js');
-          storage.storeToken(res);
-          const { accessToken } = res;
-          const decoded = decode(accessToken,{complete:true});
+      var res = JSON.parse(await response.text());
+      var storage = require('../tokenStorage.js');
+      storage.storeToken(res);
+      const { accessToken } = res;
+      const decoded = decode(accessToken, { complete: true });
 
-          try
-          {
-            var ud = decoded;
-            var userId = ud.userId;
-            var firstName = ud.firstName;
-            var lastName = ud.lastName;
+      try {
+        var ud = decoded;
+        var userId = ud.userId;
+        var firstName = ud.firstName;
+        var lastName = ud.lastName;
 
-             if( userId <= 0 )
-             {
-                setMessage('User/Password combination incorrect');
-             }
-             else
-             {
-               var user = {firstName:firstName,lastName:lastName,id:userId}
-               localStorage.setItem('user_data', JSON.stringify(user));
-  
-               setMessage('');
-               navigate("/home");
-             }
-          }
-          catch(e)
-          {
-            console.error(e);
-            return("");
-          }
+        if (userId <= 0) {
+          setMessage('User/Password combination incorrect');
+        }
+        else {
+          var user = { firstName: firstName, lastName: lastName, id: userId }
+          localStorage.setItem('user_data', JSON.stringify(user));
+
+          setMessage('');
+          navigate("/home");
+        }
       }
-      catch(e)
-      {
+      catch (e) {
         console.error(e);
-        return("");
-      }    
+        return ("");
+      }
+    }
+    catch (e) {
+      console.error(e);
+      return ("");
+    }
   };
-  
-    return(
-      <Container className="justify-content-md-center" style={{marginTop: "20vh"}}>
-        <Container style={{ justifyContent: "center", alignItems: "center", height: "40vh", width:500}} className="bg-light text-black p-5">
-          <Form onSubmit={doLogin}>
-            <h1>Sign In</h1><br />
-              <Form.Group className="mb-3" controlId="formGroupUser" >
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="Enter username" ref={loginName}/>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formGroupPassword" >
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" ref={loginPassword}/>
-              </Form.Group>
-              <Button variant="primary" type="submit">Submit</Button>
-          </Form>
-          <br />
-          <p>Don't have an account? Make one <a href="/register">here</a>!</p>
-          <span id="loginResult">{message}</span>
-        </Container>
+
+  return (
+    <Container className="justify-content-md-center" style={{ marginTop: "15vh" }}>
+      <Container style={{ justifyContent: "center", alignItems: "center", height: "70vh", width: "30vw", backgroundColor: '#8C8C8C', borderRadius: '5px' }} className="p-5">
+        <Form onSubmit={doLogin} style={{fontSize: '24px'}}>
+          <h1>Sign In</h1><br /><br />
+          <Form.Group className="mb-3" controlId="formGroupUser" >
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Enter username" style={{fontSize: '24px'}} ref={loginName} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formGroupPassword" >
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" style={{fontSize: '24px'}} ref={loginPassword} />
+          </Form.Group>
+          <Button variant="primary" className="button-block" type="submit">Submit</Button>
+        </Form>
+        <br />
+        <p>Don't have an account? Make one <a href="/register">here</a>!</p>
+        <span id="loginResult">{message}</span>
       </Container>
-    );
+    </Container>
+  );
 };
 
 export default Login;

699ac6cc05cc0e58e8f7ba68a6627f8a4eebda28
Julia Volheim <143303084+jcvol@users.noreply.github.com>
2023-11-09 16:50:55 -0500

    added new pages

---
+++ frontend/src/pages/GamePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Game from '../components/Game';
+import NavBar from '../components/NavBar';
+
+const GamePage = () =>
+{
+    return(
+        <div>
+            <NavBar />
+            <Game />
+        </div>
+    );
+};
+export default GamePage;
--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import NavBar from "../components/NavBar"
 import Welcome from '../components/Welcome';
+import NewReel from '../components/NewReel';
+import Popular from '../components/Popular';
+import NewFromFriends from '../components/NewFromFriends';
 
 const LandingPage = () => {
   return (
@@ -8,6 +11,10 @@ const LandingPage = () => {
       <style>{'body { background-color: #343434; }'}</style>
       <NavBar />
       <Welcome />
+      <Popular />
+      <NewReel />
+      <NewFromFriends />
+      <NewReel />
     </div>
   );
 };
